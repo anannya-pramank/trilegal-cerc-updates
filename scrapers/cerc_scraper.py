6f3eef735a0feb589d28d8adc773fd67272c7e68
anannya-pramank <i-anannya.pramank@trilegal.com>
2026-06-09 18:22:40 +0530

    Update cerc_scraper.py

--- scrapers/cerc_scraper.py
+++ scrapers/cerc_scraper.py
@@ -1,5 +1,4 @@
 import requests
-import winreg
 from bs4 import BeautifulSoup
 import json
 from datetime import datetime, timedelta
@@ -8,15 +7,14 @@ from io import BytesIO
 import hashlib
 import os
 
-BASE_URL = "http://cercind.gov.in"
+BASE_URL = "https://cercind.gov.in"
 ORDERS_URL = f"{BASE_URL}/recent_orders.html"
 LOOKBACK_DAYS = 7
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
     "Accept-Language": "en-US,en;q=0.9",
-    "Connection": "keep-alive"
 }
 
 def get_windows_proxy():
